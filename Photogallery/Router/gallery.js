db58d688f68e352ead9abdb1f98c938f6b7c04e7
DivyanshuTenguriya <divyanshutenguriya@gmail.com>
2026-02-12 10:20:24 +0530

    [Divyanshu] add: Photo GAllery

---
+++ Photogallery/Router/gallery.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+router.get("/", (req, res) => {
+    
+    const files = fs.readdirSync('StaticFiles/images');
+    const page =parseInt(req.query.page) ||1;
+    const limit=10;
+    const totalPage=Math.ceil(files.length/limit);
+    const startIndex=(page-1)*limit;
+    const endIndex=page*limit;
+    const paginatedFiles=files.slice(startIndex,endIndex);
+    res.render("gallery", { image: paginatedFiles, currentPage: page, totalPage: totalPage });
+});
+
+module.exports = router;
